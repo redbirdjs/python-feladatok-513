cd1857aeebf59a73172df268dd7fc30d88cb8951
Mátrai Gergő <matraigergo.1124@gmail.com>
2023-10-17 12:29:16 +0200

    xdd

--- String műveletek/kozepso.py
+++ String műveletek/kozepso.py
@@ -1,6 +1,6 @@
 inp = input("Kérek egy szót: ")
 
 if len(inp) % 2 == 0:
-    print(f"{inp[int(len(inp)/2)-1:3]}")
+    print(f"{inp[int(len(inp)/2)-1:int(len(inp)/2)+1]}")
 else:
     print(f"{inp[int(len(inp)/2)]}")
